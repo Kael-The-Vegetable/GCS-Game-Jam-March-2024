8f83b72f9c279e9af2ae8de167b29d3e433efe1c
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-03-02 13:31:40 -0700

    pedestrian walking

--- Game Jammy/Assets/Scripts/Enemy Scripts/Pedestrian.cs
+++ Game Jammy/Assets/Scripts/Enemy Scripts/Pedestrian.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Pedestrian : Actor
 {
-    public Vector3 targetDestination;
+    public List<Vector3> walkingLoop;
+    private int walkLoopIndex = 0;
 
+    public override void Start()
+    {
+        base.Start();
+    }
     void Update()
     {
-        Move(targetDestination);
+        switch (state)
+        {
+            case ActorState.Idle: 
+
+                break;
+            case ActorState.Walking:
+                if (Vector3.Distance(transform.position, walkingLoop[walkLoopIndex]) < 1)
+                { walkLoopIndex++; }
+                if (walkLoopIndex == walkingLoop.Count)
+                { walkLoopIndex = 0; }
+
+                Move(walkingLoop[walkLoopIndex]);
+                break;
+            case ActorState.Panic: 
+
+                break;
+            case ActorState.Attack: 
+
+                break;
+        }
     }
 }
