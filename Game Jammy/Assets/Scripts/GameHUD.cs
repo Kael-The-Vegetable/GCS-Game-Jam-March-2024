7034b12aecd3c96854a8779e39d4eaadb6b213c7
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-03-03 03:00:32 -0700

    menu

--- Game Jammy/Assets/Scripts/GameHUD.cs
+++ Game Jammy/Assets/Scripts/GameHUD.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameHUD : MonoBehaviour
 {
@@ -11,28 +12,40 @@ public class GameHUD : MonoBehaviour
         Playing,
         GameOver
     }
-    private GameState _state;
+    public GameState state { get; private set; }
     private TextMeshProUGUI _message;
+    private Image _menuOverlay;
+    private GameObject _button;
+    private TextMeshProUGUI _buttonText;
     void Awake()
     {
-        _state = GameState.MainMenu;
-        _message = FindObjectOfType<TextMeshProUGUI>();
+        state = GameState.MainMenu;
+        _message = GameObject.Find("Message").GetComponent<TextMeshProUGUI>();
+        _menuOverlay = GameObject.Find("MenuOverlay").GetComponent<Image>();
+        _button = GameObject.Find("Button");
+        _buttonText = _button.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     
     void Update()
     {
-        switch(_state)
+        switch(state)
         {
             case GameState.MainMenu:
-                _message.text = "<size=60>Is it Big WEEVIL?</size>\n<size=20>or small peeople?</size>";
+                _message.text = "<size=60>Is it Big WEEVIL?</size>\n<size=20>or smol ppl?</size>";
+                _menuOverlay.enabled = true;
                 break;
             case GameState.Playing:
-
+                _menuOverlay.enabled = false;
+                _button.SetActive(false);
                 break;
             case GameState.GameOver:
 
                 break;
         }
+    }
+    public void Play()
+    {
+        state = GameState.Playing;
     }
 }
--- Game Jammy/Assets/Scripts/WorldManager.cs
+++ Game Jammy/Assets/Scripts/WorldManager.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 
 public class WorldManager : MonoBehaviour
 {
+    #region Fields
+    private GameObject _player;
+    private GameHUD _hud;
+    #endregion
+
+    #region Properties
     public static WorldManager Global { get; private set; }
     public float Gravity { get; private set; } = -9.81f;
     public Vector3 PlayerPos { get; private set; }
-    private GameObject _player;
+    public bool isPlaying 
+    { 
+        get { return _hud.state == GameHUD.GameState.Playing; }
+    }
+    #endregion
+
     private void Awake()
     {
         if ( Global is not null && Global != this)
@@ -17,6 +28,7 @@ public class WorldManager : MonoBehaviour
         { Global = this; }
         Gravity = Global.Gravity;
         _player = GameObject.FindGameObjectWithTag("Player");
+        _hud = GameObject.FindGameObjectWithTag("HUD").GetComponent<GameHUD>();
     }
     private void Update()
     {
