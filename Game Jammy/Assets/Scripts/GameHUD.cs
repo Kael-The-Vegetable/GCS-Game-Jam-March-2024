ea6da08141aa5dab7a6a893167cad782a79a5c35
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-03-03 02:24:17 -0700

    a

--- Game Jammy/Assets/Scripts/GameHUD.cs
+++ Game Jammy/Assets/Scripts/GameHUD.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameHUD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum GameState
     {
-        
+        MainMenu,
+        Playing,
+        GameOver
+    }
+    private GameState _state;
+    private TextMeshProUGUI _message;
+    void Awake()
+    {
+        _state = GameState.MainMenu;
+        _message = FindObjectOfType<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        switch(_state)
+        {
+            case GameState.MainMenu:
+                _message.text = "<size=60>Is it Big WEEVIL?</size>\n<size=20>or small peeople?</size>";
+                break;
+            case GameState.Playing:
+
+                break;
+            case GameState.GameOver:
+
+                break;
+        }
     }
 }
