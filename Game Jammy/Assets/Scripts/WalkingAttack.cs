0555ce4b2e73cb09f945aababc5c6e6e484cc009
Alex Twin <alex.twintheone@gmail.com>
2024-03-03 06:22:39 -0700

    ThhyEndISNow

--- Game Jammy/Assets/Scripts/WalkingAttack.cs
+++ Game Jammy/Assets/Scripts/WalkingAttack.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WalkingAttack : Attack
 {
+    public AudioSource stepSound;
     public override void OnAttack()
     {
         base.OnAttack();
@@ -11,6 +12,8 @@ public class WalkingAttack : Attack
 
     public void OnStep()
     {
+        stepSound.pitch = Random.Range(0.5f,1.2f);
+        stepSound.Play();
         CameraShaker.Invoke(new Vector3(0.2f, 0.2f, 0.2f), new Vector3(0.2f, 0.2f, 0.2f));
     }
 }
