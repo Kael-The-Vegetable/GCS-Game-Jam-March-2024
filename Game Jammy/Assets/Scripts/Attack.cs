434356a958801443d4124e85e2c7faf33cbb94c3
Alex Twin <alex.twintheone@gmail.com>
2024-03-02 23:56:13 -0700

    ANIAMTIAONT ASOSM WOOOORK

--- Game Jammy/Assets/Scripts/Attack.cs
+++ Game Jammy/Assets/Scripts/Attack.cs
@@ -7,9 +7,12 @@ public abstract class Attack : MonoBehaviour
 
     public BoxCollider _hitBox;
     public float attackCooldown;
-    private List<Collider> _hitList = new List<Collider>();
+    public bool canAttack;
     public int _attackDamage = 10;
+
+
     private float _currentAttackCooldown;
+    private List<Collider> _hitList = new List<Collider>();
 
 
     private void Start()
@@ -24,29 +27,40 @@ public abstract class Attack : MonoBehaviour
         {
             _currentAttackCooldown -= Time.deltaTime;
             _currentAttackCooldown = Mathf.Clamp(_currentAttackCooldown, 0, attackCooldown);
+            canAttack = false;
+        }
+        else
+        {
+            canAttack = true;
         }
     }
+
+    public void AttackFrame()
+    {
+        Collider[] hits = Physics.OverlapBox(_hitBox.transform.position, _hitBox.size / 2, _hitBox.transform.rotation);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (_hitList.Contains(hits[i]) == false)
+            {
+                IDamageable damageable = hits[i].GetComponent<IDamageable>();
+
+                if (damageable != null)
+                {
+                    _hitList.Add(hits[i]);
+                    damageable.TakeDamage(_attackDamage);
+                }
+            }
+        }
+        _currentAttackCooldown = attackCooldown;
+    }
+
 
     public virtual void OnAttack()
     {
         if (_currentAttackCooldown == 0)
         {
-            Collider[] hits = Physics.OverlapBox(_hitBox.transform.position, _hitBox.size / 2, _hitBox.transform.rotation);
-
-            for (int i = 0; i < hits.Length; i++)
-            {
-                if (_hitList.Contains(hits[i]) == false)
-                {
-                    IDamageable damageable = hits[i].GetComponent<IDamageable>();
-
-                    if (damageable != null)
-                    {
-                        _hitList.Add(hits[i]);
-                        damageable.TakeDamage(_attackDamage);
-                    }
-                }
-            }
-            _currentAttackCooldown = attackCooldown;
+            
         }
     }
 }
