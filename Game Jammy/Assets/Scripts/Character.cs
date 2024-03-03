0fb2c13c1864c9953626fda1ccd0101e34b82be6
Alex Twin <alex.twintheone@gmail.com>
2024-03-02 17:19:07 -0700

    Yes

--- Game Jammy/Assets/Scripts/Character.cs
+++ Game Jammy/Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Character : MonoBehaviour, IDamageable
@@ -7,14 +8,18 @@ public class Character : MonoBehaviour, IDamageable
     public CharacterController controller;
     public float speed;
     public int maxHealth;
-    public float jumpCooldown;
+    public float stompCooldown;
+    public ParticleSystem walkingParticles;
 
     private float _currentCooldown;
     private int _currentHealth;
-    private Vector3 _moveDirection;
     private bool _isGrounded = false;
-    private bool _canJump = false;
+    private bool _canStomp = false;
     private bool _alive;
+    private Vector3 _moveDirection;
+    private Vector3 _velocity;
+    private Vector3 _lastVelocity;
+    private Vector3 _downwardVel;
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +30,31 @@ public class Character : MonoBehaviour, IDamageable
     // Update is called once per frame
     void Update()
     {
-        if (_alive) // if alive then you an do all this.
+        if (_alive) // if alive then you can do all this.
         {
             // if the character is grounded and the cooldown is 0, then the character can jump
-            _canJump = _isGrounded && _currentCooldown == 0;
-
+            _canStomp = _isGrounded && _currentCooldown == 0;
             _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
-            controller.Move(IsoNormalize(_moveDirection) * speed * Time.deltaTime);
+            if (_canStomp)
+            {
+                Stomp();
+            }
+
+            Debug.Log(controller.isGrounded);
+            _moveDirection = IsoNormalize(_moveDirection);
+            if (_moveDirection.magnitude == 1 && controller.isGrounded)
+            {
+                Debug.Log("Play");
+                walkingParticles.Play();
+            }
+            else
+            {
+                walkingParticles.Stop();
+            }
+
+            _velocity = _moveDirection * speed * Time.deltaTime;
+            transform.LookAt(transform.position + _moveDirection);
 
             if (_currentHealth <= 0)
             {
@@ -44,12 +66,27 @@ public class Character : MonoBehaviour, IDamageable
         {
             Gravity();
         }
+        else
+        {
+            
+            _downwardVel = Vector3.zero;
+        }
+
+
+        controller.Move(Vector3.Slerp(_lastVelocity, _velocity, 0.5f));
+        _lastVelocity = _velocity;
     }
     private void Gravity()
     {
         float gravity = WorldManager.Global.Gravity;
-        Vector3 downwardVel = Vector3.up * gravity * Time.deltaTime;
-        controller.Move((controller.velocity + downwardVel) * Time.deltaTime);
+        _downwardVel = Vector3.up * gravity * Time.deltaTime;
+        _downwardVel = (controller.velocity + _downwardVel) * Time.deltaTime;
+    }
+
+
+    void Stomp()
+    {
+        _currentCooldown = stompCooldown;
     }
 
     Vector3 IsoNormalize(Vector3 dir)
