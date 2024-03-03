a6cd97360124f17362d5349885620db42037d50a
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-03-02 19:29:48 -0700

    woops

--- Game Jammy/Assets/Scripts/Enemy Scripts/Pedestrian.cs
+++ Game Jammy/Assets/Scripts/Enemy Scripts/Pedestrian.cs
@@ -37,10 +37,12 @@ public class Pedestrian : Actor
                         RandomPoint(BoundingBoxPos, boundingBoxSize, out _randomWalkTowards);
                     } while (Vector3.Distance(transform.position, _randomWalkTowards) < minDistance);
                     _isAtDestination = false;
+                    agent.SetDestination(_randomWalkTowards);
+                    StartCoroutine(CheckIfStuck(1));
                 }
                 if (Vector3.Distance(transform.position, _randomWalkTowards) < 1f)
                 { _isAtDestination = true; }
-                agent.SetDestination(_randomWalkTowards);
+                
                 break;
             case ActorState.Falling:
                 break;
@@ -55,20 +57,11 @@ public class Pedestrian : Actor
                 break;
         }
     }
-    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    private IEnumerator CheckIfStuck(float timeDelay)
     {
-        for (int i = 0; i < 30; i++)
-        {
-            Vector3 randomPoint = center + Random.insideUnitSphere * range;
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
-            {
-                result = hit.position;
-                return true;
-            }
-        }
-        result = Vector3.zero;
-        return false;
+        yield return new WaitForSeconds(timeDelay);
+        if (agent.velocity.magnitude < 1)
+        { _isAtDestination = true; }
     }
     private void OnDrawGizmos()
     {
